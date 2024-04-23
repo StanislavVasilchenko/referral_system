19bb154baa8bcb949d7bbd9318bb636dbd3a2043
stanislav <stanislav.vasilchenko@yandex.ru>
2024-04-23 22:06:24 +0300

    add path login

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import RegisterAPIView
+from users.views import RegisterAPIView, LoginAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('', RegisterAPIView.as_view(), name='registration'),
+    path('login/', LoginAPIView.as_view(), name='login'),
 ]
