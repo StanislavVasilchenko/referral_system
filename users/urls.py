e9981adb8c0f674902c5c191c66ec0566fc2e872
stanislav <stanislav.vasilchenko@yandex.ru>
2024-04-23 22:34:22 +0300

    add path profile

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import RegisterAPIView, LoginAPIView
+from users.views import RegisterAPIView, LoginAPIView, UserProfileAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('', RegisterAPIView.as_view(), name='registration'),
     path('login/', LoginAPIView.as_view(), name='login'),
+    path('profile/<int:pk>/', UserProfileAPIView.as_view(), name='profile'),
 ]
