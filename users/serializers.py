9633ed2e4d75e7728cd026da1f385a9eb3b03ce4
stanislav <stanislav.vasilchenko@yandex.ru>
2024-04-23 22:05:40 +0300

    create UserLoginSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -3,7 +3,13 @@ from rest_framework import serializers
 from users.models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserRegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['phone']
+
+
+class UserLoginSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['verify_code']
