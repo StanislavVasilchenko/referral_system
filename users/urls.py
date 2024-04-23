04a5be402a8d2ca3765f7817e94d5dd08ffd0a25
stanislav <stanislav.vasilchenko@yandex.ru>
2024-04-23 23:55:19 +0300

    add path update

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import RegisterAPIView, LoginAPIView, UserProfileAPIView
+from users.views import RegisterAPIView, LoginAPIView, UserProfileAPIView, UserUpdateAPIView
 
 app_name = UsersConfig.name
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', RegisterAPIView.as_view(), name='registration'),
     path('login/', LoginAPIView.as_view(), name='login'),
     path('profile/<int:pk>/', UserProfileAPIView.as_view(), name='profile'),
+    path('update/<int:pk>/', UserUpdateAPIView.as_view(), name='profile_update'),
 ]
