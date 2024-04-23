d95364ac54908120145c03c26f1f6a4849caaee0
stanislav <stanislav.vasilchenko@yandex.ru>
2024-04-23 22:33:22 +0300

    create UserProfileSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
 from users.models import User
 
@@ -13,3 +14,15 @@ class UserLoginSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['verify_code']
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    subscriber = SerializerMethodField()
+
+    def get_subscriber(self, obj):
+        subscriber = User.objects.filter(subscribed_code=obj.invite_code)
+        return UserRegistrationSerializer(subscriber, many=True, read_only=True).data
+
+    class Meta:
+        model = User
+        fields = ['phone', 'invite_code', 'subscriber']
